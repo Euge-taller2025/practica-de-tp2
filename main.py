57b45552679e3cd164ddad607986d184b7414aa2
Eugenia Cattelan <ecattelanpedrazzini@gmail.com>
2025-04-01 23:55:53 -0300

    Archivo main con funciones de eliminar espacios y convertir en formato titulo los nombres

---
+++ main.py
@@ -0,0 +1,15 @@
+clients = [" Ana López ", "Pedro Gómez", "maria martínez", "Pedro Gómez ", "", 
+" Luis Rodríguez ", None, "ana lópez", "JUAN PÉREZ", "MARTA SUÁREZ",
+"luis rodríguez", "maría martínez ", " claudia torres", "CLAUDIA TORRES",
+" ", "pedro gómez", "Juan Pérez", None, "Ricardo Fernández", "LAURA RAMOS",
+"carlos mendes", "RICARDO FERNÁNDEZ ", " Laura ramos", "CARLOS MENDES", "alejandro gonzález", " ALEJANDRO GONZÁLEZ ", "Patricia Vega",
+"patricia VEGA", "Andrés Ocampo", " andrés ocampo", "Monica Herrera",
+"MONICA HERRERA ", "gabriela ruíz", "Gabriela Ruíz", "sandra morales",
+"SANDRA MORALES", "miguel ángel", "Miguel Ángel ", " Damián Castillo",
+"Damián Castillo ", None, "", " "]
+
+# deberia imprimir esto
+# Lista limpia de clientes al realizar todas las operaciones:
+# ['Alejandro González', 'Ana López', 'Andrés Ocampo', 'Carlos Mendes', 'Claudia Torres', 'Damián Castillo', 'Gabriela Ruíz', 'Juan Pérez', 'Laura Ramos', 'Luis Rodríguez', 'Maria Martínez', 'Miguel Ángel', 'Monica Herrera', 'Patricia Vega', 'Pedro Gómez', 'Ricardo Fernández', 'Sandra Morales']
+
+
