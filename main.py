b4f59933b4cefea33fecdf247ae564ec2a53756d
Eugenia Cattelan <ecattelanpedrazzini@gmail.com>
2025-04-01 23:57:35 -0300

    subo cambios

--- main.py
+++ main.py
@@ -13,3 +13,32 @@ clients = [" Ana López ", "Pedro Gómez", "maria martínez", "Pedro Gómez ", "
 # ['Alejandro González', 'Ana López', 'Andrés Ocampo', 'Carlos Mendes', 'Claudia Torres', 'Damián Castillo', 'Gabriela Ruíz', 'Juan Pérez', 'Laura Ramos', 'Luis Rodríguez', 'Maria Martínez', 'Miguel Ángel', 'Monica Herrera', 'Patricia Vega', 'Pedro Gómez', 'Ricardo Fernández', 'Sandra Morales']
 
 
+def clean_espacios(name):
+    """limpia los espacios extras del nombre del cliente si no es """
+    if name:
+        name = name.strip() #elimina los espacios al inicio y al final
+        name = ' '.join(name.split()) #elimina los espacios extras entre palabras
+        return name #retorna el nombre limpio
+    else: 
+        return None
+
+def conv_tit (name):
+    """convierte el nombre a titulo, primera letra con mayuscula y el resto en minusculas"""
+    if name:
+        name = name.title() #convierte el nombre a titulo
+        return name #retorna el nombre en titulo
+    else:
+        return None
+
+
+def clean_list(clients):
+    clients_cleaned =[] #creo una lista vacia donde voy a guardar la lista corregida
+    for data in clients:
+        name_cleaned=clean_espacios (data) 
+        if name_cleaned: #osea si no es vacio
+            name_cleaned= conv_tit (name_cleaned)
+            clients_cleaned.append (name_cleaned) #agrego el nombre ya limpio y procesado a la lista
+    return clients_cleaned #devuelvo la lista limpia
+
+print(clean_list(clients)) #imprimo el resultado de la funcion
+
